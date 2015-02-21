fb5e095506fc340ba713d88be9ef6a74207174f7
Patrik <pvida@diagon.com>
2015-02-21 23:09:46 +0100

    Parser for the xml model (training and test as well)

--- app/src/main/java/com/csappat/pre/biometrickeyboardid/MainActivity.java
+++ app/src/main/java/com/csappat/pre/biometrickeyboardid/MainActivity.java
@@ -3,11 +3,16 @@ package com.csappat.pre.biometrickeyboardid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.csappat.pre.biometrickeyboardid.xml.XMLParser;
+
+import org.xmlpull.v1.XmlPullParser;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -46,7 +51,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
